1b686f970c796ba9fae9f3a19f14832038d5be8d
FeSens <felipesensbonetto@gmail.com>
2024-03-09 14:37:34 -0300

    test with a huggins face transformer

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ import torch
 from torch.functional import F
 from visualizer import visualize
 
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import functools
+
+torch.set_default_device("mps")
+
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+
 def capture_shape(name, tensor, target_dict):
     if isinstance(tensor, torch.Tensor):
         target_dict[f'{name}.shape'] = tensor.shape
@@ -10,42 +20,26 @@ def capture_shape(name, tensor, target_dict):
         for i, o in enumerate(tensor):
               target_dict[f'{name}[{i}].shape'] = o.shape
 
-class MyMatMul(torch.nn.Module):
-    def __init__(self):
-        super(MyMatMul, self).__init__()
-        
-        self.layer1 = torch.nn.Linear(10, 10)
-
-    def forward(self, x):
-        x = self.layer1(x)
-        return torch.matmul(x, x.transpose(0, 1))
-
-# Example model and operation
-class MyModel(torch.nn.Module):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        
-        self.mt = MyMatMul()
-    
-    def forward(self, x):
-        x = self.mt(x)  # Operation to track
-        x = F.dropout(x, 0.5)
-        return F.softmax(x, dim=-1)
-
-target_dict = {}
 def capture_activation(name, tensor, target_dict, laye_name):
     if name == laye_name:
-        target_dict[name] = tensor
+        target_dict[name] = tensor.cpu().data.numpy().tolist()
 
 def capture_targets(name, tensor, target_dict):
     capture_shape(name, tensor, target_dict)
-    capture_activation(name, tensor, target_dict, 'mt.matmul.0')
+    capture_activation(name, tensor, target_dict, 'model.layers.2.self_attn.softmax.0')
 
-model = MyModel()
-x = torch.randn(10, 10)
+target_dict = {}
+
+inputs = tokenizer('''def print_prime(n):
+   """
+   Print all primes between 1 and n
+   """''', return_tensors="pt", return_attention_mask=False)
 
 with visualize(model, target_dict, capture_targets):
-  model(x)
+  outputs = model(**inputs)
 
-model(x)
-print(target_dict)
+
+import json
+# Save the captured shapes to a JSON file
+with open('target_dict.json', 'w') as f:
+    json.dump(target_dict, f, indent=2)
